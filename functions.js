d234e672d87a23d6e1803edb154d11626e9ec6a0
fedebacci <federicobacci1999@gmail.com>
2025-05-10 19:33:38 +0200

    pulizia test iniziali per ottenimento oggetto

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@ const myFunction = (utente) => {
     // console.log("processo.title", processo.title);
     // console.log("processo.argv", processo.argv);
 
-    // * ESEGUIRE npm run test per eseguire test
+    // * ESEGUIRE npm run test PER ESEGUIRE TEST CON PARAMETRO
     utente = JSON.parse(utente.replaceAll('\'', '\"'));
     console.log("__________________________________________________________________________________________________________________________________");
     console.log(" ");
@@ -27,51 +27,6 @@ const myFunction = (utente) => {
     console.log("typeof(idUtente) :", typeof(idUtente));
     console.log(" ");
     console.log("__________________________________________________________________________________________________________________________________");
-
-
-
-
-
-
-
-
-
-
-
-    // ! TUTTO CIO CHE VIENE SOTTO (DENTRO QUESTA FUNZIONE) E DI DEBUG E MI E SERVITO AD ARRIVARE A npm run test
-    // * ESEGUIRE npm run startWithNumberParam PER VEDERE ESEMPIO PARAMETRO
-    // * ESEGUIRE npm run startWithStringParam PER VEDERE ESEMPIO PARAMETRO STRINGA
-    // * ESEGUIRE npm run startWithObjectParam PER VEDERE ESEMPIO PARAMETRO OGGETTO (NON SEMBRA FUNZIONARE IN NESSUN MODO, MA LE ULTIME DUE FUNZIONANO SE LANCIATE DIRETTAMENTE DA CONSOLE E NON DA PACKAGE.JSON)
-    // node index.js {email:federicobacci1999@gmail.com,idUtente:0}
-    // node index.js '{email:federicobacci1999@gmail.com,idUtente:0}'
-    // node index.js '{"email":"federicobacci1999@gmail.com","idUtente":0}'
-    // node index.js \"{\"email\":\"federicobacci1999@gmail.com\",\"idUtente\":0}\"
-    
-    // node index.js {"email":"federicobacci1999@gmail.com","idUtente":0}
-    // node index.js "{\"email\":\"federicobacci1999@gmail.com\",\"idUtente\":0}"
-    // node index.js {\"email\":\"federicobacci1999@gmail.com\",\"idUtente\":0}
-        // # RISOLTO CON LA RIGA (IN PACKAGE):
-        // # "startWithObjectParam": "node index.js {'email':'federicobacci1999@gmail.com','idUtente':0}",
-        // # TRAMITE SOSTITUZIONE DI: ' IN: " (QUI IN FUNCTIONS)
-
-    
-
-    // - SEMPRE
-    // console.log("utente", utente);
-    // console.log("typeof(utente)", typeof(utente));
-
-    
-
-    // - SE startWithNumberParam
-    // console.log("parseInt(utente)", parseInt(utente));
-    // console.log("typeof(parseInt(utente))", typeof(parseInt(utente)));
-
-
-    // - SE npm run startWithObjectParam
-    // console.log("utente.replaceAll('\'', '\"')", utente.replaceAll('\'', '\"'));
-    // const test = JSON.parse(utente.replaceAll('\'', '\"'));
-    // console.log("test", test);
-    // console.log("typeof(test)", typeof(test));
 };
 
 module.exports = { myFunction };
